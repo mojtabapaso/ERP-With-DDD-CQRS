f04687afcdfa6e102e79743564f293d1070b5b87
mojtaba <mojtabapaso@gmail.com>
2025-07-01 23:10:43 +0330

    improve base entity

--- BaseCleanAPI.Domain/Common/BaseEntity.cs
+++ BaseCleanAPI.Domain/Common/BaseEntity.cs
@@ -2,9 +2,9 @@
 
 public abstract class BaseEntity
 {
-    public int Id { get; set; } 
-    public Guid RowId { get; set; } = Guid.NewGuid();
-    public bool IsDeleted { get; set; } = false;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int Id { get; set; }
+    public Guid RowId { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }

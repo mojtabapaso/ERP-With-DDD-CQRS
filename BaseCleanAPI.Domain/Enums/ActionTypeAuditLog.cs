b1c8e9c94b97f28a4406e4ddd3e01d0c076777fe
mojtaba <mojtabapaso@gmail.com>
2025-07-01 20:28:17 +0330

    +Audit

---
+++ BaseCleanAPI.Domain/Enums/ActionTypeAuditLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain.Enums;
+
+public enum ActionTypeAuditLog
+{
+    NONE = 0,
+    INSERT = 1,
+    UPDATE = 2,
+    DELETE = 3
+}
---
+++ BaseCleanAPI.Infrastructure/Persistence/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ERP.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERP.Infrastructure.Persistence.Configurations;
+
+public class AuditLogConfiguration : IEntityTypeConfiguration<Audit>
+{
+    public void Configure(EntityTypeBuilder<Audit> builder)
+    {
+        builder.ToTable("Audit", "Log");
+
+        builder.HasOne(e => e.User)
+                        .WithMany()
+                        .HasForeignKey(e => e.UserId)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(x => x.Timestamp).HasDefaultValueSql("GETDATE()");
+        builder.Property(x => x.ActionType).HasColumnType("TINYINT").HasConversion<byte>();
+        builder.Property(x => x.EntityName).HasColumnType("NVARCHAR(100)").IsRequired();
+        builder.Property(x => x.NewValues).HasColumnType("nvarchar(max)");
+        builder.Property(x => x.OldValues).HasColumnType("nvarchar(max)");
+
+    }
+}

340b8c5a436a89faa354fb0fd5b2b1dfc975ab05
mojtaba <mojtabapaso@gmail.com>
2025-10-26 22:24:39 +0330

    +AutoRegisterAttribute

---
+++ ERP.Domain/Attributes/AutoRegisterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ERP.Domain.Attributes;
+
+[AttributeUsage(AttributeTargets.Interface)]
+public class AutoRegisterAttribute : Attribute
+{
+    public ServiceLifetime Lifetime { get; }
+
+    public AutoRegisterAttribute(ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    {
+        Lifetime = lifetime;
+    }
+}
+
